a82dfbd6cfe9c9beab592572d2dfc2a4a8079797
Magnus Fiske <94929311+magnusfiske@users.noreply.github.com>
2023-03-29 13:38:25 +0200

    Update Searchbar.jsx

--- src/Layout/Searchbar.jsx
+++ src/Layout/Searchbar.jsx
@@ -2,8 +2,10 @@ import { useState } from 'react'
 import { Search } from 'react-bootstrap-icons'; 'react-bootstrap-icons';
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import './Searchbar.css'
+import ApiCall from './ApiCall'
 
 function Searchbar() {
+  const [category, setCategory] = useState("");
   return (
     <>
       <br/>
@@ -11,13 +13,15 @@ function Searchbar() {
         <Col sm={4}> 
        
         <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label className="text">Here you find recepies of the ingredients you have at home!</Form.Label>
+        <Form.Label className="text">Search food type</Form.Label>
           <Form className="d-flex">
             <Form.Control
               type="search"
-              placeholder="Type your ingredients"
+              placeholder="i.e. pasta, chicken etc"
               className="me-2"
               aria-label="Search"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
             />          
             <Button>
               <Search></Search>
@@ -27,9 +31,13 @@ function Searchbar() {
           
         </Col>
       </Row>
-
-      
-      
+<Container className="d-flex justify-content-center">
+      <Col sm={4}>
+        
+        <ApiCall food={category}/>
+        
+        </Col> 
+      </Container>
 
     </>
   );
